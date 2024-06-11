f6ff68db5eb62867a3c1f9f14e07c9860b4f09dc
akinocccc <akinochen@foxmail.com>
2024-06-11 18:39:37 +0800

    chore: update eslint config

--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,109 @@
-import * as core from '@actions/core';
-import axios from 'axios';
-import Base64 from 'js-base64';
+import * as core from '@actions/core'
+import axios from 'axios'
+import Base64 from 'js-base64'
 
 enum MessageType {
-    TEXT = 'text',
-    MARKDOWN = 'markdown',
-    IMAGE = 'image',
-    NEWS = 'news',
-    TEMPLATE_CARD = 'template_card'
-};
-type MessageTypeValue = MessageType.TEXT | MessageType.IMAGE | MessageType.MARKDOWN | MessageType.NEWS | MessageType.TEMPLATE_CARD;
+  TEXT = 'text',
+  MARKDOWN = 'markdown',
+  IMAGE = 'image',
+  NEWS = 'news',
+  TEMPLATE_CARD = 'template_card'
+}
+type MessageTypeValue =
+  | MessageType.TEXT
+  | MessageType.IMAGE
+  | MessageType.MARKDOWN
+  | MessageType.NEWS
+  | MessageType.TEMPLATE_CARD
 
-const WeComBotHookBaseUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send';
+const WeComBotHookBaseUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send'
 
 // 验证bot-key是否有效，仅检查非空
 function validateBotKey(botKey: string): boolean {
-    return botKey.trim().length > 0;
+  return botKey.trim().length > 0
 }
 
 // 发送消息到企业微信机器人的函数，支持多种消息类型
-async function sendMessageToWeComBot(botKey: string, type: MessageTypeValue, message: string): Promise<void> {
-    const url = `${WeComBotHookBaseUrl}?key=${encodeURIComponent(botKey)}`;
-    let payload;
-    
-    switch (type) {
-        case MessageType.TEXT:
-            payload = { msgtype: 'text', text: { content: message } };
-            break;
-        case MessageType.MARKDOWN:
-            payload = { msgtype: 'markdown', markdown: { content: message } };
-            break;
-        case MessageType.IMAGE:
-            // message should be base64 encoded image
-            payload = { msgtype: 'image', image: { base64: message, md5: Base64.decode(message) } }; // 需要图片的base64和md5值
-            break;
-        case MessageType.NEWS:
-            payload = { msgtype: 'news', news: { articles: JSON.parse(message) } }; // https://developer.work.weixin.qq.com/document/path/91770#图文类型
-            break;
-        case MessageType.TEMPLATE_CARD:
-            payload = { msgtype: 'template_card', template_card: JSON.parse(message) }; // https://developer.work.weixin.qq.com/document/path/91770#图文展示模版卡片
-            break;
-        default:
-            core.error('Unsupported message type')
-    }
+async function sendMessageToWeComBot(
+  botKey: string,
+  type: MessageTypeValue,
+  message: string
+): Promise<void> {
+  const url = `${WeComBotHookBaseUrl}?key=${encodeURIComponent(botKey)}`
+  let payload
 
-    core.setOutput('type', type);
-    core.setOutput('message', message);
+  switch (type) {
+    case MessageType.TEXT:
+      payload = {msgtype: 'text', text: {content: message}}
+      break
+    case MessageType.MARKDOWN:
+      payload = {msgtype: 'markdown', markdown: {content: message}}
+      break
+    case MessageType.IMAGE:
+      // message should be base64 encoded image
+      payload = {
+        msgtype: 'image',
+        image: {base64: message, md5: Base64.decode(message)}
+      } // 需要图片的base64和md5值
+      break
+    case MessageType.NEWS:
+      payload = {msgtype: 'news', news: {articles: JSON.parse(message)}} // https://developer.work.weixin.qq.com/document/path/91770#图文类型
+      break
+    case MessageType.TEMPLATE_CARD:
+      payload = {msgtype: 'template_card', template_card: JSON.parse(message)} // https://developer.work.weixin.qq.com/document/path/91770#图文展示模版卡片
+      break
+    default:
+      core.error('Unsupported message type')
+  }
 
-    try {
-        const res = await axios.post(url, payload);
-        core.info('Message sent to WeCom Bot successfully.');
-        core.setOutput('res', res);
-    } catch (error: any) {
-        core.setOutput('error', error);
-        core.error(`Failed to send message to WeCom Bot: ${error.message}`);
-    }
+  core.setOutput('type', type)
+  core.setOutput('message', message)
+
+  try {
+    const res = await axios.post(url, payload)
+    core.info('Message sent to WeCom Bot successfully.')
+    core.setOutput('res', res)
+  } catch (error: any) {
+    core.setOutput('error', error)
+    core.error(`Failed to send message to WeCom Bot: ${error.message}`)
+  }
 }
 
 async function run() {
-    try {
-        const wxWorkBotKey = core.getInput('key', { required: true });
-        if (!validateBotKey(wxWorkBotKey)) {
-            core.setFailed('Invalid or missing wecom bot hook key.');
-            return;
-        }
-
-        // 获取消息内容和消息类型
-        const messageContent = core.getInput('content', { required: true });
-        const messageType = core.getInput('type', { required: true }) as MessageTypeValue;
-        
-        // 验证消息类型
-        if (!['text', 'markdown', 'image', 'news'].includes(messageType)) {
-            core.setFailed('Invalid message type. Allowed types are "text", "markdown", "image", and "news".');
-            return;
-        }
-
-        // 发送消息
-        await sendMessageToWeComBot(wxWorkBotKey, messageType, messageContent);
-    } catch (error: any) {
-        core.setFailed(`An unexpected error occurred: ${error.message}`);
+  try {
+    const wxWorkBotKey = core.getInput('key', {required: true})
+    if (!validateBotKey(wxWorkBotKey)) {
+      core.setFailed('Invalid or missing wecom bot hook key.')
+      return
     }
+
+    // 获取消息内容和消息类型
+    const msgContent = core.getInput('content', {required: true})
+    const msgType = core.getInput('type', {
+      required: true
+    }) as MessageTypeValue
+
+    // 验证消息类型
+    if (
+      ![
+        MessageType.TEXT,
+        MessageType.IMAGE,
+        MessageType.MARKDOWN,
+        MessageType.NEWS,
+        MessageType.TEMPLATE_CARD
+      ].includes(msgType)
+    ) {
+      core.setFailed(
+        'Invalid message type. Allowed types are "text", "markdown", "image", and "news".'
+      )
+      return
+    }
+
+    // 发送消息
+    await sendMessageToWeComBot(wxWorkBotKey, msgType, msgContent)
+  } catch (error: any) {
+    core.setFailed(`An unexpected error occurred: ${error.message}`)
+  }
 }
 
-run();
+run()
